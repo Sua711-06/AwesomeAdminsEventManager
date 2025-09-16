91d6fff91d0f4a366fe370155ed90af1c73b82ad
suatii06 <suatrussell@gmail.com>
2025-09-16 10:04:11 -0300

    Started data modelling

---
+++ AwesomeAdminsTicketManager/Models/EventItem.cs
@@ -0,0 +1,13 @@
+﻿namespace AwesomeAdminsTicketManager.Models {
+    public class EventItem {
+        public int EventItemId { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public List<Tag>? Tags { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime EventTime { get; set; }
+        public string Location { get; set; } = String.Empty;
+        public string Owner { get; set; } = String.Empty;
+        public DateTime Created { get; set; }
+    }
+}
---
+++ AwesomeAdminsTicketManager/Models/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace AwesomeAdminsTicketManager.Models {
+    public class Tag {
+        public int TagId { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public int EventItemId { get; set; }
+        public EventItem? EventItem { get; set; }
+    }
+}
